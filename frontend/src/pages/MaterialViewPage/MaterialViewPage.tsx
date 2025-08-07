67d7e4e4e4e10ba7926147212e4a88066ac7ac9b
nautilus <longto.xp@gmail.com>
2025-08-07 22:23:51 +0700

    fix(material-view): fixed on-scroll image streaming with correct order

--- frontend/src/pages/MaterialViewPage/MaterialViewPage.tsx
+++ frontend/src/pages/MaterialViewPage/MaterialViewPage.tsx
@@ -4,9 +4,14 @@ import { useParams } from 'react-router-dom';
 const GET_MATERIAL_ENDPOINT = import.meta.env.VITE_GET_MATERIAL_ENDPOINT;
 const GET_MATERIAL_PAGE_ENDPOINT = import.meta.env.VITE_GET_MATERIAL_PAGE_ENDPOINT;
 
+interface IImagePage {
+	pageNumber: number;
+	imageUrl: string;
+}
+
 const MaterialViewPage = () => {
 	const [material, setMaterial] = useState(null);
-	const [imagePages, setImagePages] = useState<string[]>([]);
+	const [imagePages, setImagePages] = useState<IImagePage[]>([]);
 	const [currentPage, setCurrentPage] = useState(1);
 	const [totalPages, setTotalPages] = useState(0);
 
@@ -26,7 +31,20 @@ const MaterialViewPage = () => {
 			const imageObjectURL = URL.createObjectURL(blob);
 			console.log(imageObjectURL)
 
-			setImagePages(prevImages => [...prevImages, imageObjectURL]);
+			setImagePages(prevImages => {
+				const newImagePage: IImagePage = {
+					pageNumber: pageNumber,
+					imageUrl: imageObjectURL
+				};
+				
+				if (prevImages.some(image => image.pageNumber === pageNumber)) {
+					return prevImages.map(image => 
+						image.pageNumber === pageNumber ? newImagePage : image
+					).sort((a, b) => a.pageNumber - b.pageNumber);
+				}
+
+				return [...prevImages, newImagePage].sort((a, b) => a.pageNumber - b.pageNumber);
+			});
 		} catch (error) {
 			console.error('Error fetching material page:', error);
 		}
@@ -57,7 +75,11 @@ const MaterialViewPage = () => {
 
 	useEffect(() => {
 		if (!materialId || !currentPage) return;
-		getImagePage(currentPage);
+
+		const getImagePageAsync = async (page: number) => {
+			await getImagePage(page);
+		}
+		getImagePageAsync(currentPage);
 	}, [materialId, currentPage]);
 
 	const handleOnScroll = async (e: any) => {
@@ -65,13 +87,15 @@ const MaterialViewPage = () => {
 		const scrollHeight = e.target.scrollHeight;
 		const clientHeight = e.target.clientHeight;
 
-		console.log('Scroll Top:', scrollTop);
-		console.log('Scroll Height:', scrollHeight);
-		console.log('Client Height:', clientHeight);
-		console.log('Current Page:', currentPage);
-		console.log('Total Pages:', totalPages);
+		// console.log('Scroll Top:', scrollTop);
+		// console.log('Scroll Height:', scrollHeight);
+		// console.log('Client Height:', clientHeight);
+		// console.log('Current Page:', currentPage);
+		// console.log('Total Pages:', totalPages);
 
-		if (scrollTop >= scrollHeight * 0.5 && currentPage < totalPages) {
+		if (scrollTop + clientHeight >= scrollHeight - 10 && currentPage < totalPages) {
+			console.log('Loading next page:', currentPage + 1);
+			
 			setCurrentPage(prevPage => prevPage + 1);
 		}
 	}
@@ -83,7 +107,7 @@ const MaterialViewPage = () => {
 				{material && imagePages.length !== 0 && (
 					imagePages.map((image, index) => (
 						<div key={index} className="material-page">
-							<img src={image} alt={`Page ${index + 1}`} />
+							<img src={image.imageUrl} alt={`Page ${image.pageNumber}`} />
 						</div>
 					))
 				)}
